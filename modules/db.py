d89781c30146a67693c58b57db6b20d4934d967e
akulij <48361256+akulij@users.noreply.github.com>
2022-11-23 22:25:41 +0500

    add edit and sale product getters

--- modules/db.py
+++ modules/db.py
@@ -27,14 +27,29 @@ def _test():
     for product in session.scalars(q):
         print(product)
 
-def get_product_info(product_id: int):
+def get_product_info(product_id: int) -> ProductInfo:
     return db_utils.get_product_info(session, product_id, session_uri)
 
-def get_db_product_ids():
+def get_db_product_ids() -> list[int]:
     q = select(Product.id).where(Product.active == 1)
     products = session.scalars(q).all()
 
     return products
+
+def get_db_sale_product_ids() -> list[int]:
+    q = select(Product.id).where((Product.active == 1), (Product.dSale_SN == 1))
+    products = session.scalars(q).all()
+
+    return products
+
+def get_db_edit_product_ids() -> list[int]:
+    # q = select(Product.id).where((Product.active == 1), (Product.dSale_SN == 1))
+    q = select(Product.id).join(ProductFlags, Product.id == ProductFlags.id).where((ProductFlags.update_flag == 1), (Product.active == 1))
+    # q = select(ProductFlags.id).where(ProductFlags.update_flag == 1)
+    products = session.scalars(q).all()
+
+    return products
+# print(get_db_edit_product_ids())
 
 def get_product_picture_links(product_id: int):
     return db_utils.get_product_picture_links(session, product_id)
