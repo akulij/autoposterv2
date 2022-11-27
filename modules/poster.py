00850f038a41332b95e5c81e4d186cc39c18124e
akulij <48361256+akulij@users.noreply.github.com>
2022-11-27 14:47:17 +0500

    cycle posting till product is posted

--- modules/poster.py
+++ modules/poster.py
@@ -39,21 +39,24 @@ async def publish_to_telegram(product: ProductInfo, is_sale: bool = False) -> tu
         pictures.append(InputMediaPhoto(link, caption=msg if idx == 0 else None, parse_mode="HTML"))
     pictures = pictures[:4]
     # message = await random.choice(bots).send_photo(chat_id, photo=photo_url, caption=msg, reply_markup=keyboard, parse_mode="MARKDOWN")
-    try:
-        messages = await random.choice(bots).send_media_group(CHAT_ID, pictures)
-        if not is_sale: set_renew_flag(True)
-        time.sleep(2)
-        caption_message = messages[0]
-        photo_messages = messages[1:]
-        chat_id = caption_message.chat.id
-        msg_id = caption_message.message_id
-        photo_infos = []
-        for photo_msg in photo_messages:
-            photo_infos.append((photo_msg.chat.id, photo_msg.message_id))
+    # if True:
+    while True:
+        try:
+            messages = await random.choice(bots).send_media_group(CHAT_ID, pictures)
+            if not is_sale: set_renew_flag(True)
+            time.sleep(4)
+            caption_message = messages[0]
+            photo_messages = messages[1:]
+            chat_id = caption_message.chat.id
+            msg_id = caption_message.message_id
+            photo_infos = []
+            for photo_msg in photo_messages:
+                photo_infos.append((photo_msg.chat.id, photo_msg.message_id))
 
-        return chat_id, msg_id, msg, photo_infos
-    except:
-        pass
+            return chat_id, msg_id, msg, photo_infos
+        except:
+            print("sleeping for 30 seconds...")
+            time.sleep(30)
 
 async def renew_telegram_post(chat_id: int, msg_id: int, product: ProductInfo, prev_text: str, photo_infos: list[tuple[int, int]], is_sale: bool = False):
     builder = build_message if not is_sale else build_sale_message
