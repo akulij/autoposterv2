936879c50c4071bba23ebcd113e539eb3c3c9453
akulij <48361256+akulij@users.noreply.github.com>
2022-12-01 17:33:34 +0500

    create sale post deleter and editor

--- modules/provider.py
+++ modules/provider.py
@@ -4,22 +4,26 @@ from .db import (
         get_db_product_ids,
         get_db_edit_product_ids,
         get_db_sale_product_ids,
+        set_edited,
         )
 from .storer import (
         get_posted_product_ids,
         is_product_posted,
+        is_sale_product_posted,
         add_post,
         add_sale_post,
         get_product_post,
         get_sale_product_post,
         get_posted_sale_product_ids,
-        delete_sale_post,
+        delete_sale_post as db_delete_sale_post,
         get_nophoto_sale_posts,
         get_sale_messages,
         get_prepost_info,
         create_prepost,
         get_preposts,
         delete_prepost,
+        get_sale_product_message,
+        get_db_unmatching_date_product_ids,
         )
 from .poster import (publish_to_telegram,
         renew_telegram_post,
@@ -40,10 +44,26 @@ def get_new_products():
 
     # return new_products
 
+def get_removed_sale_product_ids():
+    posted_products = get_posted_product_ids()
+    db_products = get_db_product_ids()
+    removed_ids = list(set(posted_products) - set(db_products))
+
+    return removed_ids
+
 
 def get_edit_products():
     edit_product_ids = get_db_edit_product_ids()
     for product_id in edit_product_ids:
+        product = get_product_info(product_id)
+        yield product
+
+def get_edit_sale_products():
+    edit_product_ids = get_db_edit_product_ids()
+    unmatching_date_ids = get_db_unmatching_date_product_ids()
+    product_ids = set(edit_product_ids)
+    product_ids.union(unmatching_date_ids)
+    for product_id in product_ids:
         product = get_product_info(product_id)
         yield product
 
@@ -52,25 +72,27 @@ def get_sale_products():
     posted_products = get_posted_sale_product_ids()
     db_products = get_db_sale_product_ids()
     new_sale_product_ids = list(set(db_products) - set(posted_products))
-    new_sale_product_ids = []
     for product_id in new_sale_product_ids:
         product = get_product_info(product_id)
         # new_products.append(product)
         yield product
 
-
-async def delete_sale_products():
-    for product_id, chat_id, msg_id in get_sale_messages():
+async def delete_sale_post(product_id: int):
+    for chat_id, msg_id in get_sale_product_message(product_id):
         try:
             await delete_telegram_message(chat_id, msg_id)
         except:
             pass
-        delete_sale_post(product_id)
+        db_delete_sale_post(product_id)
         for chat_id, msg_id in get_nophoto_sale_posts(product_id, chat_id):
             try:
                 await delete_telegram_message(chat_id, msg_id)
             except:
                 pass
+
+async def delete_sale_products():
+    for product_id, _, _ in get_sale_messages():
+        await delete_sale_post(product_id)
 
 
 async def make_post(product: ProductInfo):
@@ -80,10 +102,11 @@ async def make_post(product: ProductInfo):
     else:
         chat_id, msg_id, post_text, photo_infos = get_product_post(product.id)
         await renew_telegram_post(chat_id, msg_id, product, post_text, photo_infos)
+        set_edited(product.id)
 
 
 async def make_sale_post(product: ProductInfo):
-    if not is_product_posted(product.id):
+    if not is_sale_product_posted(product.id):
         chat_id, msg_id, msg_text, photo_infos = await publish_to_telegram(product, is_sale=True)
         add_sale_post(product.id, chat_id, msg_id, msg_text, photo_infos)
     else:
--- modules/storer.py
+++ modules/storer.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from peewee import (
     SqliteDatabase,
     Model,
@@ -44,6 +45,9 @@ def get_posted_sale_product_ids() -> list[int]:
 def is_product_posted(product_id: int):
     return PostId.select().where(PostId.product_id == product_id).exists()
 
+def is_sale_product_posted(product_id: int):
+    return SalePostId.select().where(SalePostId.product_id == product_id).exists()
+
 
 def add_post(product_id: int, chat_id: int, message_id: int, message_text: str, photo_infos: list[tuple[int, int]]):
     PostId.create(product_id=product_id, chat_id=chat_id, message_id=message_id)
@@ -51,7 +55,8 @@ def add_post(product_id: int, chat_id: int, message_id: int, message_text: str, 
         PostPhoto.create(product_id=product_id, chat_id=info[0], message_id=info[1])
 
 def add_sale_post(product_id: int, chat_id: int, message_id: int, message_text: str, photo_infos: list[tuple[int, int]]):
-    SalePostId.create(product_id=product_id, chat_id=chat_id, message_id=message_id)
+    date = datetime.strftime(datetime.now(), "%d-%m-%Y")
+    SalePostId.create(product_id=product_id, chat_id=chat_id, message_id=message_id, date_posted=date)
     for info in photo_infos:
         SalePostPhoto.create(product_id=product_id, chat_id=info[0], message_id=info[1])
 
@@ -61,6 +66,13 @@ def get_sale_messages():
         msgs.append((msg.product_id, msg.chat_id, msg.message_id))
 
     return msgs
+
+def get_sale_product_message(product_id: int) -> list[tuple[int, int]]:
+    q = SalePostId.select().where((SalePostId.product_id==product_id))
+    posts = []
+    for post in q:
+        posts.append((post.chat_id, post.message_id))
+    return posts
 
 def get_nophoto_sale_posts(product_id: int, chat_id: int) -> list[tuple[int, int]]:
     q = SalePostPhoto.select().where((SalePostPhoto.product_id==product_id), (SalePostPhoto.chat_id == chat_id))
@@ -80,7 +92,7 @@ def get_product_post(product_id: int) -> tuple[int, int, str, list[tuple[int, in
     q = PostPhoto.select().where(PostPhoto.product_id==product_id)
     photo_infos = []
     for photo in q:
-        photo_infos.append((photo.chat_id, photo.msg_id))
+        photo_infos.append((photo.chat_id, photo.message_id))
     return post.chat_id, post.message_id, "", photo_infos
 
 def get_sale_product_post(product_id: int) -> tuple[int, int, str, list[tuple[int, int]]]:
@@ -147,3 +159,58 @@ def set_sale_post_format(format_text: str):
     SalePostFormat.delete().execute()
     SalePostFormat.create(format_text=format_text)
 
+def _add_user_unsafe(user_id: int):
+    User.create(
+        id=user_id,
+        state="",
+    )
+
+def is_user_exists(user_id: int) -> bool:
+    is_exists = User.select().where(User.id == user_id).exists()
+    return is_exists
+
+def add_user(user_id: int):
+    if not is_user_exists(user_id):
+        _add_user_unsafe(user_id)
+
+def new_user(user_id: int):
+    is_new_user = is_user_exists(user_id)
+    add_user(user_id)
+    return is_new_user
+
+def add_admin(user_id: int):
+    Admin.create(user_id=user_id)
+
+def is_user_admin(user_id: int):
+    return Admin.select().where(Admin.user_id ==  user_id).exists()
+
+def set_user_state(user_id: int, state: str):
+    User.update(state=state).where(User.id == user_id).execute()
+
+def get_user_state(user_id: int) -> str:
+    user = User.select().where(User.id == user_id).get()
+    return user.state
+
+def is_sale_post_date_actual(product_id: int, chat_id: int):
+    # , (SalePostId.date_posted == datetime.now().date)
+    try:
+        post = SalePostId.select().where((SalePostId.product_id==product_id), (SalePostId.chat_id == chat_id)).get()
+    except:
+        return False
+    if not post:
+        return False
+    else:
+        date = datetime.now()
+        print(f"{post.date_posted}")
+        date_posted = datetime.strptime(post.date_posted, "%Y-%m-%d")
+        return all([
+            date_posted.day == date.day,
+            date_posted.month == date.month,
+            date_posted.year == date.year,
+            ])
+
+def get_db_unmatching_date_product_ids():
+    products = SalePostId.select()
+    for product in products:
+        if not is_sale_post_date_actual(product.product_id, product.chat_id):
+            yield product
